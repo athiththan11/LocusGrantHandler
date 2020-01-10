b73765ef416385d65faff9c5ebe1977c0b24381e
athiththan11 <code19111995@gmail.com>
2020-01-10 12:56:56 +0530

    error handling

--- src/main/java/com/grant/sample/handler/LocusGrantHandler.java
+++ src/main/java/com/grant/sample/handler/LocusGrantHandler.java
@@ -9,6 +9,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
+import org.wso2.carbon.identity.oauth2.ResponseHeader;
 import org.wso2.carbon.identity.oauth2.model.RequestParameter;
 import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
 import org.wso2.carbon.identity.oauth2.token.handlers.grant.AbstractAuthorizationGrantHandler;
@@ -51,6 +52,11 @@ public class LocusGrantHandler extends AbstractAuthorizationGrantHandler {
                 locusUser.setUserName(keyValue);
                 tokReqMsgCtx.setAuthorizedUser(locusUser);
                 tokReqMsgCtx.setScope(tokReqMsgCtx.getOauth2AccessTokenReqDTO().getScope());
+            } else {
+                ResponseHeader responseHeader = new ResponseHeader();
+                responseHeader.setKey("LocusGrantError");
+                responseHeader.setValue("Invalid Credentials");
+                tokReqMsgCtx.addProperty("RESPONSE_HEADERS", new ResponseHeader[] { responseHeader });
             }
         }
 
